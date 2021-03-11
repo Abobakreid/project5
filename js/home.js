9e6a5095f9cd6331dad3d20c5240ed65482a3e67
Abo Bakr Eid <you@example.com>
2021-03-11 18:17:30 +0200

    project 5

---
+++ js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  /* start the header */
+	$(".head").css({
+   
+     height: $(window).height(),
+
+	});
+	/* end the header */
+   /* start wow.js */
+    
+    new WOW().init();
+    
+  /* end wow*/
+});
